7153b943c437906a5f0719cb750ea9f3a45b96ab
MKoesters <17874544+MKoesters@users.noreply.github.com>
2021-06-17 12:27:43 +0200

    Fix bug when mapping lower case sequences and fix case when peptide cant be mapped

--- unify_idents/engine_parsers/base_parser.py
+++ unify_idents/engine_parsers/base_parser.py
@@ -11,23 +11,6 @@ from unify_idents import UnifiedRow
 # get from params
 DELIMITER = "<|>"
 
-"""All engines
-        * Retention Time (s) is correctly set using _ursgal_lookup.pkl
-          During mzML conversion to mgf the retention time for every spec
-          is stored in a internal lookup and used later for setting the RT.
-        * All modifications are checked if they were given in
-          params['modifications'], converted to the name that was given
-          there and sorted according to their position.
-        * Fixed modifications are added in 'Modifications', if not reported
-          by the engine.
-        * The monoisotopic m/z for for each line is calculated (uCalc m/z),
-          since not all engines report the monoisotopic m/z
-        * Mass accuracy calculation (in ppm), also taking into account that
-          not always the monoisotopic peak is picked
-        * Rows describing the same PSM (i.e. when two proteins share the
-          same peptide) are merged to one row.
-"""
-
 
 class __BaseParser:
     def __init__(self, input_file, params=None):
@@ -39,7 +22,9 @@ class __BaseParser:
         self.param_mapper = uparma.UParma()
         self.peptide_mapper = UPeptideMapper(params["database"])
         self.mod_mapper = UnimodMapper()
+
         self.map_mods(self.params["Modifications"])
+
         self.scan_rt_path = self.params.get("scan_rt_lookup_file", None)
         self.scan_rt_lookup = self.read_rt_lookup_file(self.scan_rt_path)
 
@@ -54,13 +39,14 @@ class __BaseParser:
         )
 
         # mapping peptides
-        mapped = self.peptide_mapper.map_peptides([row["Sequence"]])
+        starts = []
+        ids = []
+        stops = []
+        pre = []
+        post = []
+        # we need to convert sequences to uppercase, e.g. omssa reports modified AAs in lowercase
+        mapped = self.peptide_mapper.map_peptides([row["Sequence"].upper()])
         for seq, data_list in mapped.items():
-            starts = []
-            ids = []
-            stops = []
-            pre = []
-            post = []
             for data in data_list:
                 ids.append(data["id"])
                 starts.append(str(data["start"]))
