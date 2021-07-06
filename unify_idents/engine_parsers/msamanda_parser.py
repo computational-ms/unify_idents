591deb86631dc93804aaa73fc104c6bb5c950351
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2021-07-06 17:07:53 +0200

    + get_column_names functionality

--- unify_idents/engine_parsers/msamanda_parser.py
+++ unify_idents/engine_parsers/msamanda_parser.py
@@ -25,34 +25,9 @@ class MSamandaParser(__BaseParser):
             self.reader = None
 
         self.style = "msamanda_style_1"
-        # self.column_mapping = self.get_column_names()
+        self.column_mapping = self.get_column_names()
 
-    #     # self.omssa_mod_corrections = {
-    #     #     # this dict holds corrections of wrong OMSSA to unimod assignments
-    #     #     "TMT 6-plex on K": {
-    #     #         "unimod_id": "737",
-    #     #         "ommsa_unimod_id": "738",  # this is TMT duplex in unimod
-    #     #         "unimod_name": "TMT6plex",
-    #     #     },
-    #     #     "TMT 6-plex on n-term peptide": {
-    #     #         "unimod_id": "737",
-    #     #         "ommsa_unimod_id": "738",  # this is TMT duplex in unimod
-    #     #         "unimod_name": "TMT6plex",
-    #     #         "aa_targets": ["N-term"],  # override 'X' in OMSSA mods xml
-    #     #     },
-    #     #     "TMT duplex on K": {
-    #     #         "unimod_id": "738",
-    #     #         "ommsa_unimod_id": "738",  # this is TMT duplex in unimod
-    #     #         "unimod_name": "TMT2plex",
-    #     #     },
-    #     #     "TMT duplex on n-term peptide": {
-    #     #         "unimod_id": "738",
-    #     #         "ommsa_unimod_id": "738",  # this is TMT duplex in unimod
-    #     #         "unimod_name": "TMT2plex",
-    #     #         "aa_targets": ["N-term"],  # override 'X' in OMSSA mods xml
-    #     #     },
-    #     # }
-    #
+    
     #     self.cols_to_remove = [
     #         "proteinacc_start_stop_pre_post_;",
     #         "Start",
@@ -83,7 +58,7 @@ class MSamandaParser(__BaseParser):
     #
     @classmethod
     def file_matches_parser(cls, file):
-        # TO.DO implement file sensing
+        # TODO implement file sensing
         # use get column names
         msamanda_version = "#version: 2.0.0.17442"
         with open(file) as fh:
@@ -91,34 +66,34 @@ class MSamandaParser(__BaseParser):
             if reader.fieldnames[0] == msamanda_version:
                 ret_val = True
             else:
-                ret_val = False    
+                ret_val = False   
         return ret_val
 
-    # def __iter__(self):
-    #     return self
-    #
-    # def __next__(self):
-    #     n = next(self.reader)
-    #     u = self._unify_row(n)
-    #     return u
-    #
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        n = next(self.reader)
+        u = self._unify_row(n)
+        return u
+    
     # def _unify_row(self, row):
-    #
+    
     #     new_row = {}
-    #     for unify_name, omssa_name in self.column_mapping.items():
-    #         new_row[unify_name] = row[omssa_name]
-    #     for col in self.cols_to_remove:
-    #         del new_row[col]
-    #     for col in self.cols_to_add:
-    #         new_row[col] = ""
-    #     new_row["Spectrum ID"] = int(new_row["Spectrum Title"].split(".")[1])
-    #     new_row["Search Engine"] = "omssa_2_1_9"
-    #
-    #     modstring = self.create_mod_string(new_row)
-    #     new_row["Modifications"] = modstring
-    #     new_row = self.general_fixes(new_row)
-    #
-    #     return UnifiedRow(**new_row)
+        # for unify_name, omssa_name in self.column_mapping.items():
+        #     new_row[unify_name] = row[omssa_name]
+        # for col in self.cols_to_remove:
+        #     del new_row[col]
+        # for col in self.cols_to_add:
+        #     new_row[col] = ""
+        # new_row["Spectrum ID"] = int(new_row["Spectrum Title"].split(".")[1])
+        # new_row["Search Engine"] = "omssa_2_1_9"
+    
+        # modstring = self.create_mod_string(new_row)
+        # new_row["Modifications"] = modstring
+        # new_row = self.general_fixes(new_row)
+    
+        # return UnifiedRow(**new_row)
     #
     # def create_mod_string(self, new_row):
     #     fixed_mods = []
@@ -152,133 +127,108 @@ class MSamandaParser(__BaseParser):
     #     all_mods = ";".join([":".join(m) for m in all_mods])
     #     return all_mods
     #
-    # def get_column_names(self):
-    #     # create own uparma mapper
-    #     headers = self.param_mapper.get_default_params(style=self.style)[
-    #         "header_translations"
-    #     ]["translated_value"]
-    #     return headers
-    #
-    # def _load_omssa_xml(self):
-    #     """Parsing through omssa mods to map omssa mods on unimods"""
-    #     self.omssa_mod_mapper = {}
-    #
-    #     def _create_empty_tmp():
-    #         tmp = {
-    #             "aa_targets": [],
-    #         }
-    #         return tmp
-    #
-    #     tmp = _create_empty_tmp()
-    #     xml_path = Path(self.params["omssa_mod_dir"])
-    #
-    #     xml_names = ["mods.xml", "usermods.xml"]
-    #     for xml_name in xml_names:
-    #         omssa_xml = Path(xml_path) / xml_name
-    #         # self.print_info(
-    #         #     "Parsing omssa xml ({0})".format(omssa_xml), caller="__ini__"
-    #         # )
-    #         for event, element in xml.etree.ElementTree.iterparse(omssa_xml):
-    #             if element.tag.endswith("MSModSpec_residues_E"):
-    #                 tmp["aa_targets"].append(element.text)
-    #
-    #             elif element.tag.endswith("MSMod"):
-    #                 tmp["omssa_id"] = element.text
-    #                 # tmp['MSMod'] = element.text # OMSSA ID!
-    #             elif element.tag.endswith("MSModSpec_psi-ms"):
-    #                 tmp["unimod_name"] = element.text
-    #             elif element.tag.endswith("MSModSpec_unimod"):
-    #                 tmp["unimod_id"] = element.text
-    #                 # tmp['MSModSpec_psi-ms'] = element.text # UNIMOD Name
-    #             elif element.tag.endswith("MSModSpec_name"):
-    #                 tmp["omssa_name"] = element.text
-    #                 additional = []
-    #                 if "protein" in tmp["omssa_name"]:
-    #                     additional.append("Prot")
-    #                 if "n-term" in tmp["omssa_name"]:
-    #                     additional.append("N-term")
-    #                 elif "c-term" in tmp["omssa_name"]:
-    #                     additional.append("C-term")
-    #                 if len(additional) > 0:
-    #                     tmp["aa_targets"].append("-".join(additional))
-    #
-    #             elif element.tag.endswith("MSModSpec"):
-    #                 lookup_field = "unimod_id"
-    #                 try:
-    #                     l_value = tmp[lookup_field]
-    #                 except:
-    #                     tmp["aa_targets"] = []
-    #                     continue
-    #                 if tmp["omssa_name"] in self.omssa_mod_corrections.keys():
-    #                     l_value = self.omssa_mod_corrections[tmp["omssa_name"]][
-    #                         "unimod_id"
-    #                     ]
-    #                     # for TMT mods OMSSA writes an 'X' as amino acid target, this breaks later code...
-    #                     if (
-    #                         "aa_targets"
-    #                         in self.omssa_mod_corrections[tmp["omssa_name"]].keys()
-    #                     ):
-    #                         tmp["aa_targets"] = self.omssa_mod_corrections[
-    #                             tmp["omssa_name"]
-    #                         ]["aa_targets"]
-    #                 if l_value not in self.omssa_mod_mapper.keys():
-    #                     self.omssa_mod_mapper[l_value] = {}
-    #                 self.omssa_mod_mapper[l_value][tmp["omssa_id"]] = {
-    #                     "aa_targets": tmp["aa_targets"],
-    #                     "omssa_name": tmp["omssa_name"],
-    #                 }
-    #
-    #                 tmp = _create_empty_tmp()
-    #     return
-    #
-    # def create_mod_lookup(self):
-    #     self._load_omssa_xml()
-    #     self.lookups = {}
-    #     for param_key in ["_fixed_mods", "_opt_mods"]:
-    #         mod_type = param_key[1:4]
-    #         modifications = ""
-    #         self.params[param_key] = ""
-    #         for mod in self.params["mods"][mod_type]:
-    #             unimod_id_does_not_exist = False
-    #             aa_can_not_be_mapped = True
-    #             if mod["id"] not in self.omssa_mod_mapper.keys():
-    #                 unimod_id_does_not_exist = True
-    #             else:
-    #                 if mod["aa"] == "*":
-    #                     search_target = [
-    #                         mod["pos"],
-    #                     ]
-    #                 else:
-    #                     search_target = [
-    #                         mod["aa"],
-    #                     ]
-    #                 for omssa_id in self.omssa_mod_mapper[mod["id"]].keys():
-    #                     if (
-    #                         search_target
-    #                         == self.omssa_mod_mapper[mod["id"]][omssa_id]["aa_targets"]
-    #                     ):
-    #                         modifications += "{0},".format(omssa_id)
-    #                         aa_can_not_be_mapped = False
-    #                         omssa_name = self.omssa_mod_mapper[mod["id"]][omssa_id][
-    #                             "omssa_name"
-    #                         ]
-    #                         self.lookups[omssa_name] = {
-    #                             "name": mod["name"],
-    #                             "aa_targets": self.omssa_mod_mapper[mod["id"]][
-    #                                 omssa_id
-    #                             ]["aa_targets"],
-    #                             "omssa_id": omssa_id,
-    #                             "id": mod["id"],
-    #                         }
-    #             if unimod_id_does_not_exist or aa_can_not_be_mapped:
-    #                 logger.warning(
-    #                     """
-    # The combination of modification name and aminoacid is not supported by
-    # OMSSA. Continuing without modification: {0}
-    #                 """.format(
-    #                         mod
-    #                     ),
-    #                 )
-    #                 continue
-    #
-    #         self.params[param_key] = modifications.strip(",")
+    def get_column_names(self):
+        # create own uparma mapper
+        headers = self.param_mapper.get_default_params(style=self.style)[
+            "header_translations"
+        ]["translated_value"]
+        return headers
+    
+
+# def output_cleanup(udict):
+#     cached_msamanada_output = []
+#     result_file = open(f"{udict['output_filenames'][0]}", "r")
+#     csv_dict_reader_object = csv.DictReader(
+#         (row for row in result_file if not row.startswith("#")), delimiter="\t"
+#     )
+#     headers = csv_dict_reader_object.fieldnames
+#     header_translations = udict["translated_cparameters"]["header_translations"][
+#         "translated_value"
+#     ]
+#     header_translations = {value: key for key, value in header_translations.items()}
+#     translated_headers = []
+#     for header in headers:
+#         translated_headers.append(header_translations.get(header, header))
+#     translated_headers += ["Raw data location"]
+#     logger.info("[ PARSING  ] Loading unformatted MS Amanda results ...")
+#     for line_dict in csv_dict_reader_object:
+#         cached_msamanada_output.append(line_dict)
+#     logger.info("Loading unformatted MS Amanda results done!")
+#     result_file.close()
+
+#     # self.params["output_file"] = self.params["output_file"].replace("tsv", "csv")
+#     if sys.platform == "win32":
+#         lineterminator = "\n"
+#     else:
+#         lineterminator = "\r\n"
+#     with open(f"{udict['output_filenames'][0]}", "w") as result_file:
+#         csv_dict_writer_object = csv.DictWriter(
+#             result_file,
+#             fieldnames=translated_headers,
+#             lineterminator=lineterminator,
+#         )
+#         csv_dict_writer_object.writeheader()
+#         logger.info("Writing MS Amanda results, this can take a while...")
+#         csv_write_list = []
+#         total_docs = len(cached_msamanada_output)
+#         for cache_pos, m in enumerate(cached_msamanada_output):
+#             tmp = {}
+#             for header in headers:
+#                 translated_header = header_translations.get(header, header)
+#                 tmp[translated_header] = m[header]
+#             tmp["Sequence"] = tmp["Sequence"].upper()
+
+#             if cache_pos % 500 == 0:
+#                 logger.info(
+#                     "[ INFO ] Processing line number:    {0}/{1}".format(
+#                         cache_pos, total_docs
+#                     ),
+#                     end="\r",
+#                 )
+
+#             dict_2_write = copy.deepcopy(tmp)
+#             translated_mods = []
+#             # N-Term(Acetyl|42.010565|fixed);M1(Oxidation|15.994915|fixed);M23(Oxidation|15.994915|fixed)
+#             if dict_2_write["Modifications"] != "":
+#                 splitted_Modifications = dict_2_write["Modifications"].split(";")
+#                 for mod in splitted_Modifications:
+
+#                     (
+#                         position_or_aa_and_pos_unimod_name,
+#                         mod_mass,
+#                         fixed_or_opt,
+#                     ) = mod.split("|")
+#                     (
+#                         position_or_aa_and_pos,
+#                         unimod_name,
+#                     ) = position_or_aa_and_pos_unimod_name.split("(")
+#                     position_or_aa_and_pos = position_or_aa_and_pos.strip()
+#                     unimod_name = unimod_name.strip()
+
+#                     if position_or_aa_and_pos.upper() == "N-TERM":
+#                         position = 0
+#                     else:
+#                         position = position_or_aa_and_pos[1:]
+
+#                     translated_mods.append("{0}:{1}".format(unimod_name, position))
+
+#             dict_2_write["Modifications"] = ";".join(translated_mods)
+#             dict_2_write["Raw data location"] = udict["ufiles"][0]
+
+#             # protein_id = tmp['proteinacc_start_stop_pre_post_;']
+
+#             csv_write_list.append(dict_2_write)
+#         duplicity_buffer = set()
+#         for final_dict_2_write in csv_write_list:
+#             duplicity_key = (
+#                 final_dict_2_write["Sequence"],
+#                 final_dict_2_write["Modifications"],
+#                 final_dict_2_write["proteinacc_start_stop_pre_post_;"],
+#                 final_dict_2_write["Spectrum ID"],
+#             )
+#             if duplicity_key not in duplicity_buffer:
+#                 csv_dict_writer_object.writerow(final_dict_2_write)
+#                 duplicity_buffer.add(duplicity_key)
+#     logger.info("Writing MS Amanda results done!")
+#     return
+
