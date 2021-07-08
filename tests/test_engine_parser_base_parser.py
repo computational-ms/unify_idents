0d6108030de89cee6ea3156ef894cd4a3836c7b5
MKoesters <17874544+MKoesters@users.noreply.github.com>
2021-07-08 16:37:54 +0200

    add simple test for base parser

---
+++ tests/test_engine_parser_base_parser.py
@@ -0,0 +1,23 @@
+from unify_idents.engine_parsers.msfragger3_parser import __BaseParser
+from pathlib import Path
+
+
+def test_engine_parsers_xtandem_init():
+    input_file = (
+        Path(__file__).parent / "data" / "test_Creinhardtii_QE_pH11_xtandem_alanine.xml"
+    )
+    rt_lookup_path = Path(__file__).parent / "data" / "_ursgal_lookup.csv.bz2"
+    db_path = Path(__file__).parent / "data" / "test_Creinhardtii_target_decoy.fasta"
+
+    parser = __BaseParser(
+        input_file,
+        params={
+            "rt_pickle_name": rt_lookup_path,
+            "database": db_path,
+            "modifications": [
+                "C,fix,any,Carbamidomethyl",
+                "M,opt,any,Oxidation",
+                "*,opt,Prot-N-term,Acetyl",
+            ],
+        },
+    )
